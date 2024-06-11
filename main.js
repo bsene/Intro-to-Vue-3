89e5ba24a83da433b3f957ddc09425da451b2bdf
Birame S. <birame.sene@zenika.com>
2024-06-12 00:10:31 +0200

    resolved 1st challenge

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-const product = 'Socks'
+
+const app = Vue.createApp({
+    data() {
+        return {
+            product: "Socks",
+        }
+    },
+    computed: {
+        description() {
+            return `Amazing ${this.product} !`
+        }
+    }
+});
