aa6ab60e29ab22e37df617daf8023d3dd42e37ee
Birame S. <birame.sene@zenika.com>
2024-06-13 10:22:58 +0200

    challenge 5 resolved

--- main.js
+++ main.js
@@ -4,7 +4,8 @@ const app = Vue.createApp({
             product: 'Socks',
             image: './assets/images/socks_blue.jpg',
             inStock: true,
-            details: ['50% cotton', '30% wool', '20% polyester']
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            sizes: [27,28,29,30,31]
         }
     }
 })
